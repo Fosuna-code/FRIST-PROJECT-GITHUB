b0b69986641ef77f6a6c05ba16863813e8187aa3
Fernando <ferosuna2001@gmail.com>
2019-09-01 20:19:48 -0600

    aqui anexe el cambio de color random del contenedor

---
+++ modulo.js
@@ -0,0 +1,21 @@
+
+var boton = document.getElementById("change_color_container"); 
+boton.addEventListener("click", change_color); 
+
+function alerta(){
+    alert("hola")   
+}
+
+function change_color(){
+    var contenedor = document.getElementById("blog-post_container");
+    var color_continer_html = document.getElementById("colores_rgb");
+    var color_continer = "rgb(";
+    color_continer = color_continer + (Math.random()*255).toFixed(2) + ","; 
+    color_continer = color_continer + (Math.random()*255).toFixed(2) + ",";
+    color_continer = color_continer + (Math.random()*255).toFixed(2) + ")";
+    
+    contenedor.style.color = color_continer;
+    color_continer_html.innerHTML = color_continer;
+    
+}
+
